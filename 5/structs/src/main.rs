f6f37e3f30fcae1b5740fea6500ae328d2c12867
michel.aquino <michel.aquino@g.globo>
2021-10-30 16:56:10 -0300

    Struct update syntax

--- 5/structs/src/main.rs
+++ 5/structs/src/main.rs
@@ -8,6 +8,7 @@ struct User {
 fn main() {
     imutable_struct();
     mutable_struct();
+    struct_update_syntax();
 }
 
 fn mutable_struct() {
@@ -25,6 +26,20 @@ fn imutable_struct() {
     println!("email: {}", user1.email);
     println!("sign_in_count: {}", user1.sign_in_count);
     println!("active: {}", user1.active);
+}
+
+fn struct_update_syntax() {
+    let user1 = build_user(String::from("someone@example.com"), String::from("someusername123"));
+    let user2 = User {
+        email: String::from("user2@example.com"),
+        ..user1
+    };
+
+    println!("");
+    println!("username: {}", user2.username);
+    println!("email: {}", user2.email);
+    println!("sign_in_count: {}", user2.sign_in_count);
+    println!("active: {}", user2.active);
 }
 
 fn build_user(email: String, username: String) -> User {
