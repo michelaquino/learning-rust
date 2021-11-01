d497358599887e08a8329679123f760ce98b6ab5
michel.aquino <michel.aquino@g.globo>
2021-11-01 17:08:47 -0300

    Information inside enum on match

--- 6/6_2_match_control_flow/src/main.rs
+++ 6/6_2_match_control_flow/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     println!("The value of {:?} is {}", Coin::Penny, value_in_cents(&Coin::Penny));
     println!("The value of {:?} is {}", Coin::Nickel, value_in_cents(&Coin::Nickel));
     println!("The value of {:?} is {}", Coin::Dime, value_in_cents(&Coin::Dime));
-    println!("The value of {:?} is {}", Coin::Quarter, value_in_cents(&Coin::Quarter));
+    println!("The value of {:?} is {}", Coin::Quarter(UsState::Alabama), value_in_cents(&Coin::Quarter(UsState::Alabama)));
 }
 
 fn value_in_cents(coin: &Coin) -> u8 {
@@ -15,14 +15,33 @@ fn value_in_cents(coin: &Coin) -> u8 {
             println!("Dime matched");
             10
         },
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            match state {
+                UsState::Alabama => {
+                    println!("Alabama");
+                }
+                UsState::Alaska => {
+                    println!("Alaska");
+                }
+            }
+            
+            return 25
+        },
     }
 }
+
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
 
 #[derive(Debug)]
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
