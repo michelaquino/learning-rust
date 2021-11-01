a56ab10145644c3c36642896990dbd6dc3311929
michel.aquino <michel.aquino@g.globo>
2021-11-01 14:40:24 -0300

    refactoring with tuples

--- 5/5_2/rectangles/src/main.rs
+++ 5/5_2/rectangles/src/main.rs
@@ -1,10 +1,16 @@
 fn main() {
     let widht = 30;
     let height = 50;
+    let dimensions = (30, 50);
 
     println!("The area of retangle is {}", area(widht, height));
+    println!("[Tuple] The area of retangle is {}", area_tuple(dimensions));
 }
 
 fn area(widht: i32, height: i32) -> i32 {
     return widht * height
+}
+
+fn area_tuple(dimensions: (i32, i32)) -> i32 {
+    return dimensions.0 * dimensions.1
 }
