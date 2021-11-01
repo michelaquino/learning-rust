afc2c474fe87a871036e8e6d31d82dc6b27f1660
michel.aquino <michel.aquino@g.globo>
2021-11-01 15:50:29 -0300

    Example with enums

---
+++ 6/enums/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    let version4 = IpAddrKind::V4;
+    let version6 = IpAddrKind::V6;
+
+    println!("Vertsion 4: {:?}", version4);
+    println!("Vertsion 6: {:?}", version6);
+    
+    let localhost = IpAddress{
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+    
+    let loopback = IpAddress{
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    println!("localhost: {:?}", localhost);
+    println!("loopback: {:?}", loopback);
+}
+
+#[derive(Debug)]
+struct IpAddress {
+    kind: IpAddrKind,
+    address: String
+}
+
+#[derive(Debug)]
+enum IpAddrKind {
+    V4,
+    V6,
+}
