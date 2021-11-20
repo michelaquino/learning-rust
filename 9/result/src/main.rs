5602335b0ceab9f139c19456cfdfebd201264925
michel.aquino <michel.aquino@g.globo>
2021-11-20 17:16:19 -0300

    Propagating errors

--- 9/result/src/main.rs
+++ 9/result/src/main.rs
@@ -1,11 +1,13 @@
+use std::fs;
 use std::fs::File;
-use std::io::ErrorKind;
+use std::io::{self, ErrorKind, Read};
 
 fn main() {
     // handle_result(); 
     // handle_result_with_different_errors();
     // using_unwarap();
     using_expected();
+    propagating_errors();
 }
 
 fn using_unwarap() {
@@ -39,4 +41,42 @@ fn handle_result_with_different_errors() {
             _unknonwn_error => panic!("Problem opening the file: {:?}", error),
         }
     };
+}
+
+fn propagating_errors() -> Result<String, io::Error>{
+    let file_path = "file.txt";
+    let f = File::open(file_path);
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e)
+    };
+
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => return Ok(s),
+        Err(err) => return Err(err),
+    }
+}
+
+/*
+The ? at the end of the File::open call will return the value inside an Ok to the variable f. If an error occurs, the ? operator will return early out of the whole function and give any Err value to the calling code. The same thing applies to the ? at the end of the read_to_string call.
+*/
+fn propagating_errors_interrogation_operator() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn propagating_errors_interrogation_operator_shorter() -> Result<String, io::Error> {
+    let mut s = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    Ok(s)
+}
+
+fn propagating_errors_interrogation_operator_even_shorter() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
 }
